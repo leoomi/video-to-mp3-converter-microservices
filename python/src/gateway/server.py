779763ca2a0e5e5f4374d084688a776f462f0eaf
Leo Omi <leoyuuki@gmail.com>
2023-07-14 13:36:25 -0300

    Most services are done

---
+++ python/src/gateway/server.py
@@ -0,0 +1,53 @@
+import os, gridfs, pika, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+
+fs = gridfs.GridFS(mongo_video.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+
+@server.route("/upload", methods=["POST"])
+def upload():
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        if len(request.files) > 1 or len(request.files) < 1:
+            return "exactly 1 file required", 400
+
+        for _, f in request.files.items():
+            err = util.upload(f, fs, channel, access)
+
+            if err:
+                return err
+
+        return "success!", 200
+
+    return "not authorized", 401
+
+@server.route("/download", methods=["GET"])
+def download():
+    pass
+
+if __name__ == "__main__":
+    server.run("0.0.0.0", port=8080)
